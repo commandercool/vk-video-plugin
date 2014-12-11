f95556cd1764731449bc039e29cbd0cb53868f5a
commandercool <erohin_a_v@mail.ru>
2014-12-11 14:35:50 +0300

    New quality string added: 480p; added support for search result links.

--- vk.groovy
+++ vk.groovy
@@ -5,16 +5,21 @@ import groovy.json.*
 /**
 * <h1>vk.com Serviio plugin</h1>
 * 
-* @version 1
+* @version 2
 * @author commandercool
+*
+* Changes:
+* - new quality string added: 480p;
+* - added support for search result links.
 */
 
 class Vk extends WebResourceUrlExtractor {
 
-    final Integer VERSION = 1;
+    final Integer VERSION = 2;
     final String VALID_URL = /http:\/\/vk.com\/video(\d+)_(\d+)/;
+    final String VALID_SEARCH_URL = /http:\/\/vk.com\/video\?(.*)video(\d+_\d+)/;
     final String NVARS = /nvar vars = (\{.*\});/;
-    final String[] quals = ['240', '360', '720', '1080'];
+    final String[] quals = ['240', '360', '480', '720', '1080'];
 
     int getVersion() {
         return VERSION;
@@ -25,12 +30,16 @@ class Vk extends WebResourceUrlExtractor {
     }
     
     boolean extractorMatches(URL feedUrl) {
-        return feedUrl ==~ VALID_URL
+        return (feedUrl ==~ VALID_URL || feedUrl ==~ VALID_SEARCH_URL)
     }
     
     WebResourceContainer extractItems(URL resourceUrl, int maxItemsToRetrieve) {
         def container = new WebResourceContainer()
-        def http = resourceUrl.text;
+        if (resourceUrl ==~ VALID_SEARCH_URL) {
+            def urlstr = "http://vk.com/video" + (resourceUrl =~ VALID_SEARCH_URL)[0][2]
+            resourceUrl = new URL(urlstr)
+        }
+        def http = resourceUrl.text             
         def jsonstr = (http =~ NVARS)[0][1]
         jsonstr = jsonstr.replaceAll('\\\\', '')
         def json = new JsonSlurper().parseText(jsonstr)
@@ -43,11 +52,11 @@ class Vk extends WebResourceUrlExtractor {
             if (url != null) {
                 def fulltitle = title + " [${qual}p]"
                 def item = new WebResourceItem(title: fulltitle, additionalInfo: [
-                expiresImmediately: false,
-                cacheKey: fulltitle,
-                url: url,
-                thumbnailUrl: json.jpg,
-                live: false
+                    expiresImmediately: false,
+                    cacheKey: fulltitle,
+                    url: url,
+                    thumbnailUrl: json.jpg,
+                    live: false
                 ])
                 items += item        
             }
@@ -68,13 +77,11 @@ class Vk extends WebResourceUrlExtractor {
         }
         return c
     }
-
+    
     static void main(args) {
         Vk vk = new Vk()
-        def url = "http://vk.com/video3007616_164708547"
-        
+        def url = "http://vk.com/video?q=black%20keys&section=search&z=video7555330_165618061"
         println vk.extractorMatches(new URL(url))
-        
         vk.extractItems(new URL(url), 123).getItems().each { it->
             ContentURLContainer result = vk.extractUrl(it, PreferredQuality.HIGH)
             println result
